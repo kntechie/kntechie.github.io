c21053888aaedd62e4888c0f72a4875a622f2d34
kntechie <kumaran26kumaran@gmail.com>
2018-11-12 20:02:16 +0100

    react app

---
+++ my-app/precache-manifest.3f004dc8f3c5b91b3f2783729788eb1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e90d1118c793a5a74a6c",
+    "url": "/static/js/main.e90d1118.chunk.js"
+  },
+  {
+    "revision": "8c0859d6a58d61cc3a39",
+    "url": "/static/js/1.8c0859d6.chunk.js"
+  },
+  {
+    "revision": "e90d1118c793a5a74a6c",
+    "url": "/static/css/main.6993c9eb.chunk.css"
+  },
+  {
+    "revision": "589b4d3393a4e04e3bc8d3dd94ccf2ab",
+    "url": "/index.html"
+  }
+];
